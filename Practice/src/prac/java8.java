39ac176506c65d89b8f6cb4db20d021de828948c
rajkumar <rajukumarcheerla@gmail.com>
2021-08-09 12:06:35 +0530

    firstcommit

---
+++ Practice/src/prac/java8.java
@@ -0,0 +1,15 @@
+package prac;
+
+public class java8 {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=5;j<i;j--) {
+				System.out.print("0");
+			}
+			
+		}
+		System.out.println();
+		
+	}
+
+}
---
+++ Practice/src/sangeetha/datatypesss.java
@@ -0,0 +1,37 @@
+package sangeetha;
+
+public class datatypesss {
+	public static void main(String[] args) {
+		System.out.println("hugfsnv");
+		santha.geetha();
+		santha s=new santha();
+		s.mul();
+		
+		
+		
+	}
+
+}
+class santha{
+	static int add() {
+		int a=3;
+		int b=7;
+		return a+b;
+	}
+	void sub() {
+		int a=10;
+		int b=5;
+		System.out.println(a-b);
+	}
+	int mul() {
+		int a=6;
+		int b=9;
+		return a*b;
+	}
+	static void geetha() {
+		System.out.println("hi this is sangeetha welcome to java");
+	}
+	
+	
+	
+}
